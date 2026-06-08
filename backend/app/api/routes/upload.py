8907fbc72939842e6c71ac114365bc9caa94cd2e
Ayush Kumar Sahani <Ayushkrsahani2006@gmail.com>
2026-06-08 15:37:15 +0530

    Add CSV upload endpoint

--- backend/app/api/routes/upload.py
+++ backend/app/api/routes/upload.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from fastapi import UploadFile,APIRouter, File 
+import os 
 
-router = APIRouter()
+router  = APIRouter()
+UPLOAD_FOLDER = "uploads"
 
-@router.get("/")
-async def test_upload():
-    return {"message": "Upload route working"}
+os.makedirs(UPLOAD_FOLDER, exist_ok = True ) 
+
+@router.post("/")
+async def upload_csv(file:UploadFile = File(...)):
+      if not file.filename.endswith(".csv"):
+          return {"error": "Please upload a CSV file"}
+      
+      file_path = os.path.join(UPLOAD_FOLDER,file.filename) 
+      
+      with open(file_path,"wb")as buffer:
+          buffer.write(await file.read())
+      
+      return {
+        "filename": file.filename,
+        "message": "File uploaded successfully"
+      }
+          
