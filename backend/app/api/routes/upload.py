ebb8ee0abbd8cfdc86c23950af94b84e54df4a07
Ayush Kumar Sahani <Ayushkrsahani2006@gmail.com>
2026-06-08 16:18:00 +0530

    add dataset profiling after upload

--- backend/app/api/routes/upload.py
+++ backend/app/api/routes/upload.py
@@ -1,10 +1,20 @@
 from fastapi import UploadFile,APIRouter, File 
 import os 
+import pandas as pd
 
 router  = APIRouter()
 UPLOAD_FOLDER = "uploads"
 
 os.makedirs(UPLOAD_FOLDER, exist_ok = True ) 
+
+def data_profile(df):
+    return{
+        "rows" : len(df),
+        "columns" : len(df.columns),
+        "column_names" : list(df.columns),
+        "data_types":df.dtypes.astype(str).to_dict()   
+    }
+    
 
 @router.post("/")
 async def upload_csv(file:UploadFile = File(...)):
@@ -15,9 +25,10 @@ async def upload_csv(file:UploadFile = File(...)):
       
       with open(file_path,"wb")as buffer:
           buffer.write(await file.read())
-      
+      df = pd.read_csv(file_path) 
+      profile = data_profile(df)
       return {
         "filename": file.filename,
-        "message": "File uploaded successfully"
+        "profile" : profile
       }
           
