31727328024b9edde444216822d08e5ab43dc677
Ayush Kumar Sahani <Ayushkrsahani2006@gmail.com>
2026-06-12 17:52:25 +0530

    added null counting

--- backend/app/ml_engine/nodes/clean_node.py
+++ backend/app/ml_engine/nodes/clean_node.py
@@ -3,11 +3,25 @@ import pandas as pd
 def clean_dataset(file_path):
     df = pd.read_csv(file_path) 
     original_rows = len(df) 
-    df = df.drop_duplicates(df) 
+    df = df.drop_duplicates() 
     rows_after_cleaning = len(df) 
     duplicates_removed = original_rows - rows_after_cleaning 
     return {
         "original_rows": original_rows,
         "duplicates_removed": duplicates_removed,
         "rows_after_cleaning": rows_after_cleaning
+        import pandas as pd 
+
+def clean_dataset(file_path):
+    df = pd.read_csv(file_path) 
+    original_rows = len(df) 
+    df = df.drop_duplicates(df) 
+    rows_after_cleaning = len(df) 
+    duplicates_removed = original_rows - rows_after_cleaning 
+    missing_values = df.isnull().sum().to_dict()
+    return {
+        "original_rows": original_rows,
+        "duplicates_removed": duplicates_removed,
+        "rows_after_cleaning": rows_after_cleaning,
+        "missing_values" :  missing_values
     }
