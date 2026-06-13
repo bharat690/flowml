64e3c1f73079114907143ba5124cde531e70be3d
Ayush Kumar Sahani <Ayushkrsahani2006@gmail.com>
2026-06-13 18:10:48 +0530

    Add missing value handling

--- backend/app/ml_engine/nodes/clean_node.py
+++ backend/app/ml_engine/nodes/clean_node.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from pandas.api.types import is_numeric_dtype
 
 def clean_dataset(file_path):
     df = pd.read_csv(file_path) 
@@ -7,6 +8,15 @@ def clean_dataset(file_path):
     rows_after_cleaning = len(df) 
     duplicates_removed = original_rows - rows_after_cleaning 
     missing_values = df.isnull().sum().to_dict()
+
+    for column in df.columns:
+      if is_numeric_dtype(df[column]):
+         df[column] = df[column].fillna(df[column].median())
+      else:
+         df[column] = df[column].fillna(df[column].mode()[0])
+    
+    print(df.dtypes)
+
     return {
         "original_rows": original_rows,
         "duplicates_removed": duplicates_removed,
